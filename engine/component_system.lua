a365a659dc6a3d37528306948454ff4d6bc8186d
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-06-24 12:58:35 -0400

    internal ecs check & automation

--- engine/component_system.lua
+++ engine/component_system.lua
@@ -11,12 +11,19 @@ function ecs:new(object)
 
     self.__index = self
 
+    self.entity_count = 0
+
     return object
 end
 
 -- component addons
 
 function ecs:add_component(component)
+
+    assert(component ~= "entity_count", "entity_count is a reserved key word for ecs")
+
+    assert(component ~= nil or type(component) ~= "string", "ecs key word must be a string")
+
     if not self[component] then
         self[component] = {}
     end
@@ -32,6 +39,31 @@ function ecs:add_components(component_table)
     end
 end
 
+-- entity creation system
+function ecs:add_entity(component_variable_table)
+
+    -- skip having to count internal tables
+
+    self.entity_count = self.entity_count + 1
+
+    -- dynamic calculation of missing variables
+
+    local component_list = {}
+
+    for key,_ in pairs(self) do
+        table.insert(component_list, key)
+    end
+
+    -- insert defined values
+
+    for key,value in pairs(component_variable_table) do
+        self[key] = value
+
+        component_list[key] = nil
+    end
+
+end
+
 
 -- testing
 
@@ -39,13 +71,10 @@ local my_ecs = ecs:new()
 
 my_ecs:add_component("life")
 
-my_ecs:add_components({
-    "life2",
-    "documents",
-    "x",
-    "y",
-    "pos",
-    "distance"
+-- my_ecs:add_components({})
+
+my_ecs:add_entity({
+    life = true
 })
 
 
